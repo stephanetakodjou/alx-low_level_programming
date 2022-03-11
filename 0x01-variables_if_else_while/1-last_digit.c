089cce357d750d327c01ee6310cd90347d70a430
stephanetakodjou <stephanetakodjou@gmail.com>
2022-03-11 05:53:30 -0800

    fix error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,12 @@
 #include <time.h> 
 #include <stdio.h>
 /*
+ * main - Entry point
  *
- *  * main - Entry point
- *  * Description: print the value of n status:
- *   greater than, is zero and is not less than 6.
- *   Return: Always O (Success)
+ * Description: print the value of n status:
+ *		   greater than, is zero and is not less than 6.
+ * 
+ * Return: Always O (Success)
  *
  *  
  */
